de3c82bc2ed7ac55170541e191280addf35d0c13
mgeffroy <mgeffroy@heylab.lan>
2024-09-08 17:10:11 +0200

    add swagger to django project

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_spectacular.views import (
+  SpectacularAPIView,
+  SpectaculerSwaggerView,
+)
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/',
+         SpectaculerSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs'
+         )
 ]
